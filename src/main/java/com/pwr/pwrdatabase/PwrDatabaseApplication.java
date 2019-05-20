16949bfc64bcc0cb92b22e0dcbef50b37baf4dd1
Patryk Sobow <patryk.sobow95@gmail.com>
2019-05-20 04:13:41 +0200

    init

---
+++ src/main/java/com/pwr/pwrdatabase/PwrDatabaseApplication.java
@@ -0,0 +1,15 @@
+package com.pwr.pwrdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwrDatabaseApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PwrDatabaseApplication.class, args);
+    }
+
+}
